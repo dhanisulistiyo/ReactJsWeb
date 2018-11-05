045b1f3ced39bc588f8840873737e443e2e47adf
Anonymous\Dhani Sulistiyo <dhani.sulistiyo.wibowo@basajans.com>
2018-11-05 10:44:33 +0700

    add reducer in apps

--- solution/src/index.js
+++ solution/src/index.js
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
+import "./index.css";
 import "semantic-ui-css/semantic.min.css";
+import { Provider } from "react-redux";
+import store from "./config/store";
 
-ReactDOM.render(<App />, document.getElementById("root"));
-registerServiceWorker();
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
